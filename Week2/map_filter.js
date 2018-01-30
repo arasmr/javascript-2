2684521135522677a0c0d9cefca854825e147d39
emre aras <earas446@gmail.com>
2018-01-30 14:13:02 -0800

    Last commit

--- Week2/map_filter.js
+++ Week2/map_filter.js
@@ -10,15 +10,9 @@ for (let i = 0; i < numbers.length; i++) {
 console.log("The doubled numbers are", newNumbers); //[2,6]
 console.log("----------------------------------------------------------------------------");
 
-let filteredNumbers = numbers.filter((x)=> {
-    if (x % 2 !== 0) {
-        return x;
-    }
-});
+let filteredNumbers = numbers.filter(x => x % 2 !== 0)
 
-let mapNumbers = filteredNumbers.map((x)=> {
-    return x * 2;
-});
+let mapNumbers = filteredNumbers.map(y => y * 2)
 
 console.log("The doubled numbers are", mapNumbers);
 
@@ -93,6 +87,8 @@ console.log(filteredEvents);
 console.log("----------------------------------------------------------------------------");
 
 
+
+
 const payment = [];
 tasks.map((time) => {
     let hour = Math.trunc(time.duration / 60);
@@ -120,3 +116,24 @@ const reducer = (prevVan, currentValue) => prevVan + currentValue;
 console.log("The total amount of the money which Maartje will have is " + payPayment.reduce(reducer) + " Euro(s). ");
 
 console.log("----------------------------------------------------------------------------");
+
+const anotherPayment = [];
+tasks.map((newTime) => {
+    let totalTime = newTime.duration;
+    return anotherPayment.push(totalTime);
+})
+const reducer1 = (prevVal1, currentValue1) => prevVal1 + currentValue1;
+console.log((anotherPayment.reduce(reducer1) / 60) * 5);
+
+console.log("----------------------------------------------------------------------------");
+
+let totalMoney = 0;
+const earnedMoneyPerHour = 38.45;
+
+//Thanks for Jamsheed for this solution.
+tasks
+    .map(changeTime => changeTime.duration / 60)
+    .filter(filterTime => filterTime < 2)
+    .map(calculateMoney => calculateMoney * earnedMoneyPerHour)
+    .forEach(amount => totalMoney += amount)
+console.log("Maartje has earned €" + totalMoney);
