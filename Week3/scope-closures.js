a1f1e30d5beaae3cb31a1c1b15a81ab1697e4cf6
emre aras <earas446@gmail.com>
2018-02-02 16:36:01 -0800

    second homework added

---
+++ Week3/scope-closures.js
@@ -0,0 +1,21 @@
+
+let createBase = function (number) {
+    return function (y) {
+        console.log(number + y);
+    }
+}
+let addSix = createBase(6);
+
+addSix(10); // returns 16
+addSix(21); // returns 27
+
+console.log("---------------------------------------------------------------------------");
+
+let myArray = ['a', 'b', 'c', 'd', 'a', 'e', 'f', 'c'];
+
+//https://codehandbook.org/how-to-remove-duplicates-from-javascript-array/
+
+function removeDuplicate() {
+    return Array.from(new Set(myArray));
+}
+console.log(removeDuplicate());
